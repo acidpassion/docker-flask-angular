8a11ce645738fb41729dfddc64931b269bdb7a05
Alex Xie <alex.xie.micro@outlook.com>
2018-09-02 20:15:16 +0800

    added test api

--- server/app.py
+++ server/app.py
@@ -1,5 +1,5 @@
 import os
-from flask import Flask
+from flask import Flask, jsonify
 # from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -9,9 +9,28 @@ app.config.from_object(os.environ['APP_SETTINGS'])
 
 # from models import Result
 
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol', 
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web', 
+        'done': False
+    }
+]
+
 @app.route("/")
 def hello():
     return "Hello from Python!!!"
+
+@app.route('/api/v1/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks': tasks})
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
