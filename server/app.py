39b34536e9f4fb7eab95f02cf2e59b5e983757d8
Alex Xie <alex.xie.micro@outlook.com>
2018-09-27 23:30:39 +0800

    test

--- server/app.py
+++ server/app.py
@@ -32,6 +32,13 @@ def hello():
 def get_tasks():
     return jsonify({'tasks': tasks})
 
+@app.route('/api/v1/tasks/<int:task_id>', methods=['GET'])
+def get_task(task_id):
+    task = [task for task in tasks if task['id'] == task_id]
+    if len(task) == 0:
+        abort(404)
+    return jsonify({'task': task[0]})
+
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
